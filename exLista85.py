7ba0424a6d0afca048b52fd788da11bf32c4d91b
Luís Martini <90358818+luismartini@users.noreply.github.com>
2021-12-20 11:12:10 -0300

    Add files via upload

--- exLista85.py
+++ exLista85.py
@@ -3,6 +3,7 @@ Crie um programa em que o usuário possa digitar sete valores númericos e cadas
 No final mostre os valores pares e ímpares em ordem crescente.
 '''
 número = list()
+#número = [[], []]
 pares = list()
 ímpares = list()
 for n in range(0, 7):
@@ -13,3 +14,18 @@ for n in range(0, 7):
         ímpares.append(número)
 print(f'Os números pares são {sorted(pares)}')
 print(f'Os números ímpares foram {sorted(ímpares)}')
+
+'''
+núm = [[], []]
+valor = 0
+for c in range(0, 8):
+    valor = int(input('Digite um valor: '))
+    if valor % 2 == 0:
+        núm[0].append(valor)
+    else:
+        núm[1].append(valor)
+print('-' * 30)
+print(f'Todos os valores: {núm}')
+print(f'Os valores pares {núm[0]}')
+print(f'Os valores ímpares {núm[1]}')
+'''
