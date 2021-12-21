a775b61a5f207cc045339663f652ea2b4daf9601
Luís Martini <90358818+luismartini@users.noreply.github.com>
2021-12-21 19:18:00 -0300

    Add files via upload

--- exDic92.py
+++ exDic92.py
@@ -1,3 +1,16 @@
 '''
 Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 '''
+from datetime import datetime
+dados = dict()
+dados['nome'] = input('Nome: ')
+nascimento = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nascimento
+dados['CTPS'] = int(input('Carteira de trabalho (0 não tem): '))
+if dados['CTPS'] != 0:
+    dados['contratação'] = int(input('Ano de Contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + (dados['contratação'] + 35) - datetime.now().year
+print('=' * 30)
+for k, v in dados.items():
+    print(f'{k} tem o valor {v}')
--- exDic93.py
+++ exDic93.py
@@ -1,3 +1,25 @@
 '''
 Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 '''
+jogador = dict()
+partidas = list()
+média = 0
+jogador['nome'] = input('Nome: ')
+total = int(input(f'Partidas jogadas pelo {jogador["nome"]}: '))
+for c in range(0, total):
+    partidas.append(int(input(f'Gols na partida {c + 1}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+# Primeira forma
+print('=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('=' * 30)
+
+#Segunda
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+for i, v in enumerate(jogador['gols']):
+    print(f'Na partida {i+1}, fez {v} gols')
+print(f'Foram um total de {jogador["total"]} gols')
+média = jogador['total'] / 2
+print(f'A média de gols foi {média:.0f}')
