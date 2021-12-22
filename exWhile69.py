858faa8a1c419de790204e3dd1ff77139b58ee16
Luís Martini <90358818+luismartini@users.noreply.github.com>
2021-12-22 13:22:22 -0300

    Add files via upload

--- exWhile69.py
+++ exWhile69.py
@@ -4,3 +4,26 @@ A) quantas pessoas tem mais de 18 anos.
 B) quantos homens foram cadastrados.
 C) quantas mulheres tem menos de 20 anos. 
 '''
+total18 = 0
+totH = totM = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = input('Sexo: ').upper().strip()
+    if idade >= 18:
+        total18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM += 1
+
+    
+    r = ' '
+    while r not in 'SN':
+        r = input('Continuar [S/N] ?').upper().strip()
+    if r == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {total18}')
+print(f'Ao todo temos {totH} cadastrados')
+print(f'Ao todo temos {totM} mulheres menores de 20 anos')
--- exWhile70.py
+++ exWhile70.py
@@ -4,3 +4,40 @@ A) qual é o total gasto na compra.
 B) quantos produtos custam mais de R$1000.
 C) qual é o nome do produto mais barato. 
 '''
+totGasto = prodmil = menor = maior = cont = 0
+nomeBarato = ' '
+nomeCaro = ' '
+while True:
+    produto = input('Produto: ')
+    preço = float(input('Preço: R$ '))
+    cont +=1
+    #total de gastos
+    totGasto += preço
+
+    # Produtos mais caros
+    if preço >= 1000:
+        prodmil += 1
+    
+    #nome do produto mais barato
+    
+    if cont == 1:
+        menor = preço
+        nomeBarato = produto
+    else:
+        if preço < menor:
+            menor = preço
+            nomeBarato = produto
+        if preço > maior:
+            maior = preço
+            nomeCaro = produto
+    #resposta
+    resp = ' '
+    while resp not in 'SN':
+        resp = input('Continuar [S/N]? ').upper()[0]
+    if resp == 'N':
+        break
+
+print(f'O total de gastos foram R${totGasto}')
+print(f'Produtos acima de R1000 são: R${prodmil}')
+print(f'O nome mais caro é {nomeCaro}')
+print(f'O nome do produto mais barato foi {nomeBarato} e custa R${menor}')
